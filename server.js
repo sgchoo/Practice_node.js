77060071d09e37f547bc30e5744b8155b9e3e9fd
sgchoo <choo19950306@gmail.com>
2024-04-06 22:11:31 +0900

    ajax delete

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+// method-override
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 // parser
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -33,7 +37,7 @@ new MongoClient(url).connect()
     });
 
 // ====================================================
-// send html
+// Send HTML
 // ====================================================
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html');
@@ -84,12 +88,87 @@ app.post('/add', async (req, res) => {
     }
 });
 
+// ==================================================
+// Board Detail Information
+// ==================================================
+
 app.get('/detail/:id', async (req, res) => {
-    console.log(req.params);
-    // URL의 뒷부분(/detail/:id의 id)을 가지고있는 document를 가져온다
-    const result = await db.collection('sgchoo').findOne({ _id: new ObjectId(req.params.id) })
-    console.log(result);
-    // render시에 데이터를 보내준다.
-    res.render('detail.ejs', { title: result.title, content: result.content });
+    try
+    {
+        // URL의 뒷부분(/detail/:id의 id)을 가지고있는 document를 가져온다
+        const result = await db.collection('sgchoo').findOne({ _id: new ObjectId(req.params.id) })
+        // render시에 데이터를 보내준다.
+        if(result == null)
+            res.status(404).send('invalid url')
+        else
+            res.render('detail.ejs', { title: result.title, content: result.content, id: req.params.id });
+    }
+    catch(e)
+    {
+        console.log(e);
+        res.status(404).send('invalid url')
+    }
 });
 
+// ==================================================
+// Board Rewrite
+// ==================================================
+
+app.get('/rewrite/:id', async (req, res) => {
+    try
+    {
+        const result = await db.collection('sgchoo').findOne({ _id: new ObjectId(req.params.id) });
+        if(result == null)
+            res.status(404).send('invalid url');
+        else
+            res.render('rewrite.ejs', { title: result.title, content: result.content, id: req.params.id });
+    }
+    catch(e)
+    {
+        console.log(e);
+        res.status(404).send('invalid url');
+    }
+});
+
+app.put('/rewrite/:id', async (req, res) => {
+    // await db.collection('sgchoo').updateOne({ _id: 1 }, { $inc: { like: 1 } });
+
+    try
+    {
+        if(req.body.title == '' || req.body.content == '')
+            res.send("<script>alert('수정 할 내용을 입력해 주세요.');location.href='/rewrite/:id';</script>");
+        else
+        {
+            var findDoc = { _id: new ObjectId(req.params.id) }
+            var updateSrc = { $set: { title: req.body.title, content: req.body.content } };
+
+            await db.collection('sgchoo').updateOne(findDoc, updateSrc);
+            res.redirect('/list');
+        }
+    }
+    catch(e)
+    {
+        console.log(e);
+        res.status(404).send('invalid url')
+    }
+});
+
+// ==================================================
+// Board Delete
+// ==================================================
+
+app.delete('/abc', async(req, res) => {
+    console.log(req.body)
+    var findDocById = { _id: new ObjectId(req.body._id)};
+
+    const result = await db.collection('sgchoo').deleteOne(findDocById);
+
+    if(result.deletedCount === 1)
+    {
+        res.send('Delete Document Success');
+    }
+    else
+    {
+        res.status(500).send('Delete Document Failed');
+    }
+});
